fee7eff6c9413850b8d37cd42af6c5db8c8644f8
ashishkumar1550 <kumarashish1550@gmail.com>
2019-01-15 14:36:38 +0530

    course add done

--- src/courses/views.py
+++ src/courses/views.py
@@ -15,55 +15,62 @@ from firebase_admin import credentials
 from firebase_admin import auth
 
 # Create your views here.
+import json
 
 @csrf_exempt
 def add_courses(request):
 	if request.method == 'POST':
-		token = request.META['HTTP_AUTHORIZATION']
-		decoded_token = auth.verify_id_token(token)
-		uid = decoded_token['uid']
-		user = auth.get_user(uid)
-		django_user, created = User.objects.get_or_create(email = 'sodhi@iitrpr.ac.in', defaults = {
-			'username': 'balwinder',
-			'password': 'iitropar',
-		})
-		profile = django_user.profile
-		# Check if the user is a teacher
-		if profile.is_teacher == False:
-			return HttpResponseForbidden()
+		try:
+			print(request.body)
+			token = request.META['HTTP_AUTHORIZATION']
+			decoded_token = auth.verify_id_token(token)
+			uid = decoded_token['uid']
+			user = auth.get_user(uid)
+			django_user, created = User.objects.get_or_create(email = user.email, defaults = {
+				'username': user.email,
+				'password': 'iitropar',
+			})
+			profile = django_user.profile
+			# Check if the user is a teacher
+			if profile.is_teacher == False:
+				return HttpResponseForbidden()
 
-		teacher = profile.teacher
+			teacher = profile.teacher
 
-		days_list = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-		
-		course_json = request.POST # Body dekh lio kaise milti hai
-		course, created = Course.objects.get_or_create(code = course_json['course_code'], defaults = {
-			'name': course_json['course_name']})
-		ttc, created = TeachersTeachCourses.objects.get_or_create(teacher = teacher, course = course)
+			days_list = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+			
+			course_json = request.body.decode('utf-8') # Body dekh lio kaise milti hai
+			print(request.body)
+			course_json = json.loads(course_json)
+			course, created = Course.objects.get_or_create(code = course_json['course_code'], defaults = {
+				'name': course_json['course_name']})
+			ttc, created = TeachersTeachCourses.objects.get_or_create(teacher = teacher, course = course)
 
-		current_date = datetime.now()
-		for weeks in range(16):
-			# For 16 weeks
-			# Get current week
-			cur_week = current_date + timedelta(weeks = weeks)
-			for days in range(5):
-				# From monday to friday
-				timing = course_json['lectures'][days_list[days]]
-				# Get the days to add
-				days_to_add = days - current_date.day
-				days_to_add = days_to_add if days_to_add >=0 else 7 - days_to_add
-				# Get the current day
-				cur_day = cur_week + timedelta(days = days_to_add)
+			current_date = datetime.now()
+			for weeks in range(16):
+				# For 16 weeks
+				# Get current week
+				cur_week = current_date + timedelta(weeks = weeks)
+				for days in range(5):
+					# From monday to friday
+					timing = course_json['lectures'][days_list[days]]
+					# Get the days to add
+					days_to_add = days - current_date.day
+					days_to_add = days_to_add if days_to_add >=0 else 7 - days_to_add
+					# Get the current day
+					cur_day = cur_week + timedelta(days = days_to_add)
 
-				for tim in timing:
-					start_time = datetime.strptime(tim['start_time'], '%H:%M').time()
-					end_time = datetime.strptime(tim['end_time'], '%H:%M').time()
-					cur_start_datetime = datetime.combine(cur_day.date(), start_time)
-					cur_end_datetime = datetime.combine(cur_day.date(), end_time)
+					for tim in timing:
+						start_time = datetime.strptime(tim['start_time'], '%H:%M').time()
+						end_time = datetime.strptime(tim['end_time'], '%H:%M').time()
+						cur_start_datetime = datetime.combine(cur_day.date(), start_time)
+						cur_end_datetime = datetime.combine(cur_day.date(), end_time)
 
-					lecture, created = Lecture.objects.get_or_create(course = ttc, begin = cur_start_datetime, end = cur_end_datetime)
+						lecture, created = Lecture.objects.get_or_create(course = ttc, begin = cur_start_datetime, end = cur_end_datetime)
 
-		return JsonResponse({"status": "Done"})
+			return JsonResponse({"status": "Done"})
+		except:
+			return JsonResponse({'status':'failure'})
 	 
 
 
